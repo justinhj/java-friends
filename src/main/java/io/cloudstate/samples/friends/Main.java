ccfc0fa73cbce4645ec1487b0c31bafc50bd8b3a
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-05-28 13:14:34 -0700

    now compiles and runs

--- src/main/java/io/cloudstate/samples/friends/Main.java
+++ src/main/java/io/cloudstate/samples/friends/Main.java
@@ -1,11 +1,17 @@
 package io.cloudstate.samples.friends;
 
+import cloudstate.samples.chat.friends.*;
 import io.cloudstate.javasupport.*;
-//import io.cloudstate.
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		System.out.println("Horse!");
+    new CloudState()
+        .registerCrdtEntity(
+            FriendsEntity.class,
+						FriendsOuterClass.getDescriptor().findServiceByName("Friends"))
+				.start()
+        .toCompletableFuture()
+        .get();
 	}
 }
