5a02b10657d8679d2d779be824673b2937d8adfe
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-05-28 09:20:52 -0700

    wip

---
+++ src/main/java/io/cloudstate/samples/friends/Main.java
@@ -0,0 +1,11 @@
+package io.cloudstate.samples.friends;
+
+import io.cloudstate.javasupport.*;
+import cloudstate.samples.chat.friends.*;
+
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Horse!");
+	}
+}
